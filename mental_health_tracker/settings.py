227276b1c621a878499c19744d298eccf241f1a4
indahcahyaaa <indahcahyapuspita22@gmail.com>
2024-09-11 14:24:24 +0700

    nyoba commit tutor 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/indah.cahya31/mentalhealthtracker", "https://pbp.cs.ui.ac.id/web/project/indah.cahya31/mentalhealthtracker"]
+# saved
