fc74c713e14c5f1d348c40fbbc203477bb581884
indahcahyaaa <indahcahyapuspita22@gmail.com>
2024-09-17 12:49:25 +0700

    nyoba

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/indah.cahya31/mentalhealthtracker", "https://pbp.cs.ui.ac.id/web/project/indah.cahya31/mentalhealthtracker"]
 # saved
 # test
+# halo
