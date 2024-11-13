408fb7c7c68bbaa1018f6e963e20cdeb13300d95
indahcahyaaa <indahcahyapuspita22@gmail.com>
2024-11-13 14:38:09 +0700

    tutorial 8 done

--- main/views.py
+++ main/views.py
@@ -16,6 +16,9 @@ from django.shortcuts import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -24,7 +27,7 @@ def show_main(request):
 
     context = {
         'npm' : '2306245453',
-        'name': request.user.username,
+        'name': request.user.username,   
         'class': 'PBP A',
         # 'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
@@ -135,3 +138,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
